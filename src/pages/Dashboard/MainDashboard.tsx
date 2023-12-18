bd4d266f9198f1702936784e0db2801aaa318842
Mohanad Inairat <mohannad.inairat@iqvia.com>
2023-12-18 21:35:03 +0200

    fix filters

--- src/pages/Dashboard/MainDashboard.tsx
+++ src/pages/Dashboard/MainDashboard.tsx
@@ -9,44 +9,13 @@ import IconSave from '../../images/icon/icon-save.svg';
 const MainDashboard = () => {
   return (
     <>
+      <TagsInput />
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <TagsInput />
-        <div className="col-span-4 items-center flex justify-between">
-          
-        <Link
-              to="#"
-              className="inline-flex items-center justify-center gap-2.5 rounded-full bg-meta-3 py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-            >
-              <span>
-              </span>
-              Search
-            </Link>
-
-            <Link
-              to="#"
-              className="inline-flex items-center justify-center gap-2.5 rounded-full bg-black py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-            >
-              <span>
-               <img src={IconSave} />
-              </span>
-              Save
-            </Link>
-
-            <Link
-              to="#"
-              className="inline-flex items-center justify-center gap-2.5 rounded-full border border-primary py-2 px-10 text-center font-medium text-primary hover:bg-opacity-90 lg:px-8 xl:px-10"
-            >
-              <span>
-                <img src={IconClear}></img>
-              </span>
-              clear
-            </Link>
-        </div>
         <ChartFour />
         <div className="xl:col-span-3">
           <SideFilters />
         </div>
-        <div className='col-span-9'>
+        <div className="col-span-9">
           <div className="col-span-12 xl:col-span-12">
             <TableThree />
           </div>
